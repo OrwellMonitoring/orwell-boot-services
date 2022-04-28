362279e9f5a99b99bae61eec40b70dd069fe653e
alexandre <alexandreserras@ua.pt>
2022-04-28 12:20:48 +0100

    telegraf done

--- telegraf/telegraf_init.sh
+++ telegraf/telegraf_init.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-if [  -f ./ip_host ] then
-hostname  -I | cut -f1 -d' ' > ./ip_host;
-fi
-IP=$(cat ./ip_host) KAFKA=$(curl  -s '10.0.12.82:8008/service_discovery/kafka/' | python3 -c "import sys, json; print(json.load(sys.stdin)['host'])") telegraf -config /usr/bin/telegraf.conf
+IP=$(hostname  -I | cut -f1 -d' ') KAFKA=$(curl  -s $(cat /usr/bin/kafka_location.txt) | python3 -c "import sys, json; print(json.load(sys.stdin)['host'])") telegraf -config /usr/bin/telegraf.conf
