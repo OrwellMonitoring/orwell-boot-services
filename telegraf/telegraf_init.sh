2754595f7bf79188e76189cf408494e387964c84
alexandre <alexandreserras@ua.pt>
2022-04-28 14:33:57 +0100

    fix na api

--- telegraf/telegraf_init.sh
+++ telegraf/telegraf_init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IP=$(hostname  -I | cut -f1 -d' ') KAFKA=$(curl  -s $(cat /usr/bin/kafka_location.txt) | python3 -c "import sys, json; print(json.load(sys.stdin)['host'])") telegraf -config /usr/bin/telegraf.conf
+IP=$(hostname  -I | cut -f1 -d' ') KAFKA=$(curl   $(cat /usr/bin/kafka_location.txt)) telegraf -config /usr/bin/telegraf.conf
