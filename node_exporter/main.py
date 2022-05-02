af44b51431c229df04355508c37b7ddacb71d64c
goncalo-leal <goncalolealsilva@ua.pt>
2022-05-02 13:10:56 +0100

    Fix exporter

--- node_exporter/main.py
+++ node_exporter/main.py
@@ -1,26 +1,35 @@
+import os
 import json
 import time
-from datetime import datetime
-import requests 
+import socket
+import subprocess
+import requests
+from kafka import KafkaProducer
 
+# getting the IP address
+bash_command = ["hostname",  "-I"]
+process = subprocess.Popen(bash_command, stdout=subprocess.PIPE)
+ip_address = process.communicate()[0].decode("utf-8").split(" ")[0]
 
 while True:
-    f=open("kakfa_location.txt","r")
+    f=open("kafka_location.txt","r")
     kafka_url = f.read()
     response= requests.get(url=kafka_url).text
     kafka_location=response
+    print(kafka_location)
+
     metrics = requests.get(url="http://localhost:9100/metrics").text
     ts= int(time.time())
-    ## getting the IP address using socket.gethostbyname() method
-    ip_address = requests.get('https://api.ipify.org').text
-    #ip_address= socket.gethostbyname(socket.gethostname() )
     metrics= metrics.split("\n")
+
     lst=[]
     for metric in metrics :
         if metric.startswith("#") or metric == "":continue
+        
         metric = metric.split(" ")
         value=metric[-1]
         properties=metric[:-1]
+        
         if len(properties) > 1:
                 properties = "_".join(properties)
         else:
@@ -34,10 +43,18 @@ while True:
 
         properties["instance"]= ip_address
         title=array[0]
-        metric=title+str(properties)+" "+value+" "+str(ts)
-        #print(metric)
+       
+        properties = str(properties).replace(':', '=').replace(' ', '').replace('\'', '"').replace('"=', '=').replace(',"', ',').replace('{"', '{') 
+        metric=title+properties+" "+value+" "+str(ts)
         lst.append(metric)
+
     print(lst)
-    #escrever a metrica no kafka e adicionar lhe a instance
+
+    producer = KafkaProducer(
+        bootstrap_servers=[kafka_location],
+        value_serializer=lambda x: json.dumps(x).encode("utf-8")
+    )
+    producer.send('prometheus', value=lst)
+
     #mandar metrica a metrica ou a list logo toda? 
     time.sleep(15)
