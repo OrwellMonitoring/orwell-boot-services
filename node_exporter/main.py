19cdd5aedbd896311a03d3c5cf14258e6273867f
goncalo-leal <goncalolealsilva@ua.pt>
2022-05-03 17:13:32 +0100

    try except to avoid errors when the api doesn't answer

--- node_exporter/main.py
+++ node_exporter/main.py
@@ -7,6 +7,7 @@
 # Description:
 # Prometheus node exporter metrics fetch and send to kafka
 
+from nis import cat
 import os
 import json
 import time
@@ -21,48 +22,51 @@ process = subprocess.Popen(bash_command, stdout=subprocess.PIPE)
 ip_address = process.communicate()[0].decode("utf-8").split(" ")[0]
 
 while True:
-    f=open(os.path.basename('kafka_location.txt'),"r")
-    kafka_url = f.read()
-    response= requests.get(url=kafka_url).text
-    kafka_location=response
-    print(kafka_location)
+    try:
+        f=open(os.path.basename('kafka_location.txt'),"r")
+        kafka_url = f.read()
+        response= requests.get(url=kafka_url).text
+        kafka_location=response
+        print(kafka_location)
 
-    metrics = requests.get(url="http://localhost:9100/metrics").text
-    ts= int(time.time())
-    metrics= metrics.split("\n")
+        metrics = requests.get(url="http://localhost:9100/metrics").text
+        ts= int(time.time())
+        metrics= metrics.split("\n")
 
-    lst=[]
-    for metric in metrics :
-        if metric.startswith("#") or metric == "":continue
+        lst=[]
+        for metric in metrics :
+            if metric.startswith("#") or metric == "":continue
+            
+            metric = metric.split(" ")
+            value=metric[-1]
+            properties=metric[:-1]
+            
+            if len(properties) > 1:
+                    properties = "_".join(properties)
+            else:
+                properties=properties[0]
+            
+            array = properties.split("{")
+            if len(array) > 1:
+                properties = { v[0]:v[1].strip("\"}") for v in [ v.split("=") for v in array[1].split(",") ] } if len(array[1]) > 1 else {}
+            else:
+                properties=dict()
+
+            properties["instance"]= ip_address
+            title=array[0]
         
-        metric = metric.split(" ")
-        value=metric[-1]
-        properties=metric[:-1]
-        
-        if len(properties) > 1:
-                properties = "_".join(properties)
-        else:
-            properties=properties[0]
-        
-        array = properties.split("{")
-        if len(array) > 1:
-            properties = { v[0]:v[1].strip("\"}") for v in [ v.split("=") for v in array[1].split(",") ] } if len(array[1]) > 1 else {}
-        else:
-            properties=dict()
+            properties = str(properties).replace(':', '=').replace(' ', '').replace('\'', '"').replace('"=', '=').replace(',"', ',').replace('{"', '{') 
+            metric=title+properties+" "+value+" "+str(ts)
+            lst.append(metric)
 
-        properties["instance"]= ip_address
-        title=array[0]
-       
-        properties = str(properties).replace(':', '=').replace(' ', '').replace('\'', '"').replace('"=', '=').replace(',"', ',').replace('{"', '{') 
-        metric=title+properties+" "+value+" "+str(ts)
-        lst.append(metric)
-
-    print(lst)
-    
-    producer = KafkaProducer(
-        bootstrap_servers=[kafka_location],
-        value_serializer=lambda x: json.dumps(x).encode("utf-8")
-    )
-    producer.send('prometheus', value=lst)
+        print(lst)
+        
+        producer = KafkaProducer(
+            bootstrap_servers=[kafka_location],
+            value_serializer=lambda x: json.dumps(x).encode("utf-8")
+        )
+        producer.send('prometheus', value=lst)
+    except:
+        print("Error, maybe")
 
     time.sleep(15)
